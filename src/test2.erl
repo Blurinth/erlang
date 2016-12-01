55f967c270d8a09a47981de083efb816fce01335
Blurinth <olsonquinn1@gmail.com>
2016-11-30 23:52:07 -0500

    added testing with a list of 20 lists varying in sizes(0-10)

--- src/test2.erl
+++ src/test2.erl
@@ -8,7 +8,7 @@
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([sort/1]).
+-export([sort/1, testlistgen/0, testgen/1]).
 
 
 
@@ -46,19 +46,40 @@ sort({List, Swaps}, 1) when Swaps > 0->
 	sort(sortbig(List), 2);
 sort({List, Swaps}, 2) when Swaps > 0->
 	sort(sortsmall(List), 1);
-sort({List, 0}, _) ->
+sort({List, 0}, 2) ->
+	sort(sortsmall(List), 1);
+sort({List, 0}, 1) ->
 	List.
 
 sort_test() ->
-	sort_test([[],[1],[2,1],[2,3,1]]).
+	sort_test([[], [1],[2,1],[1,3,2]]).
 sort_test([A | Tests]) ->
-	Correct = list:sort(A),
+	Correct = lists:sort(A),
 	Correct = sort(A),
-	sort_test(Tests),
+	sort_test(Tests);
 sort_test([]) ->
 	ok.
 				
-		
+testlistgen() ->
+	testlistgen([], 1).
+testlistgen(Out, Tests) when Tests =< 20 ->
+	testlistgen([testgen(rand:uniform(11)-1) | Out], Tests + 1);
+testlistgen(Out, _) ->
+	Out.
+testgen(Max_size) ->
+	testgen([], Max_size - 1, 0).
+testgen(Out, Max_size, _) when Max_size == 0 ->
+	Out;
+testgen(Out, Max_size, Current_size) when Current_size =< Max_size ->
+	testgen([rand:uniform(11)-1 | Out], Max_size, Current_size + 1);
+testgen(Out, _, _) ->
+	Out.
+
+
+
+
+
+
 				
 				
 				
