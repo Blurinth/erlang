0657af8fd44615b95e84929120f9647345404f45
Blurinth <olsonquinn1@gmail.com>
2016-12-13 15:15:07 -0500

    added better sqrt function that uses bit search and works with all numbers

--- src/test3.erl
+++ src/test3.erl
@@ -2,7 +2,7 @@
 %% @doc @todo Add description to test3.
 
 -module(test3).
--export([sqr/2, sqrt/1, getstart/1]).
+-export([sqr/2, sqrt/1, getstart/1, sqrt2/1]).
 
 sqr(Num, Times) ->
 	sqr(Num, Num, Times).
@@ -32,3 +32,29 @@ sqrt(Num, Place, Test) ->
 		A * A < Num -> sqrt(Num, Place / 10, Test);
 		true -> sqrt(Num, Place, Test - Place)
 	end.
+
+sqrt2(0) ->
+	0;
+sqrt2(Num) when Num < 0 ->
+	erlang:error(badarith);
+sqrt2(Num) when Num > 1 ->
+	sqrt2(Num, 0, Num);
+sqrt2(Num) ->
+	sqrt2(Num, 0, 1).
+sqrt2(Num, Min, Max) ->
+	Mid = (Min + Max) / 2,
+	Midsqr = Mid * Mid,
+	if
+		(Mid == Min) or (Mid == Max) -> Mid;
+		Midsqr < Num -> sqrt2(Num, Mid, Max);
+		Midsqr > Num -> sqrt2(Num, Min, Mid);			
+		true -> Mid
+	end.
+
+
+
+
+
+
+
+		
