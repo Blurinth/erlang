5f7319d023f8822c720539bd19c11f41d4677552
Blurinth <olsonquinn1@gmail.com>
2016-12-18 21:49:50 -0500

    changed "badargs" or "badarg". added try-catch for get_number calling, added small unit test.

--- src/parsing.erl
+++ src/parsing.erl
@@ -1,7 +1,7 @@
 %% @author Quinn
 -module(parsing).
 -export([get_number/1, get_operator/1, main/1]).
-
+-include_lib("eunit/include/eunit.hrl").
 
 get_number(String) ->
 	get_number(String, []).
@@ -14,11 +14,11 @@ get_number([L | Ls] = List, Out) ->
 		true -> {list_to_integer(lists:reverse(Out)), List}
 	end.
 get_number_float([], Out) ->
-	{list_to_integer(lists:reverse(Out)), []};
+	{list_to_float(lists:reverse(Out)), []};
 get_number_float([L | Ls] = List, Out) ->
 	if
 		(L >= $0) and (L =< $9) -> get_number_float(Ls, [L | Out]);
-		L == $. -> erlang:error(badargs);
+		L == $. -> erlang:error(badarg);
 		true -> {list_to_float(lists:reverse(Out)), List}
 	end.
 get_operator([L | Ls]) ->
@@ -27,7 +27,9 @@ get_operator([L | Ls]) ->
 		L == $- -> {'-', Ls};
 		L == $* -> {'*', Ls};
 		L == $/ -> {'/', Ls};
-		true -> erlang:error(badargs)
+		L == $( -> {'(', Ls};
+		L == $) -> {')', Ls};
+		true -> erlang:error(badarg)
 	end.
 main(String) ->
 	main(String, []).
@@ -36,8 +38,13 @@ main([], Out) ->
 main([L | Ls] = List, Out) ->
 	if
 		(L >= $0) and (L =< $9) ->
-			{A, B} = get_number(List),
-			main(B, [A | Out]);
+			try get_number(List) of
+				_ ->
+					{A, B} = get_number(List),
+					main(B, [A | Out])
+			catch
+				error:badarg -> main(Ls, Out)
+			end;
 		L == $ -> main(Ls, Out);
 		true ->
 			try get_operator(List) of
@@ -45,19 +52,11 @@ main([L | Ls] = List, Out) ->
 					{A, B} = get_operator(List),
 					main(B, [A | Out])
 			catch
-				error:badargs -> main(Ls, Out)
+				error:badarg -> main(Ls, Out)
 			end
 	end.
-
-
-
-
-
-
-
-
-
-
-
-
-
+main_test() ->
+[1,'+',2] = main("1+2"),
+[0.45,'/',6.5] = main("0.45/6.5"),
+[0,'*',1.567] = main("0*1.567"), 
+[5,'-',0] = main("5-0"). 
