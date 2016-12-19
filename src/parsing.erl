2060c8551570648d5feaed538fee1e86c3ad6bf1
Blurinth <olsonquinn1@gmail.com>
2016-12-19 02:04:16 -0500

    added work on parentheses functions. the parentheses_nested function has bugs

--- src/parsing.erl
+++ src/parsing.erl
@@ -31,6 +31,30 @@ get_operator([L | Ls]) ->
 		L == $) -> {')', Ls};
 		true -> erlang:error(badarg)
 	end.
+get_parentheses(List) ->
+	get_parentheses(List, []).
+get_parentheses([], _) ->
+	erlang:error(badarg);
+get_parentheses([L | Ls], Out) ->
+	io:format("parent~n"),
+	if
+		L == $( -> 
+			{A, B} = get_parentheses_nested(Ls, []),
+			get_parentheses(A, [B | Out]);
+		L == $) -> {{'()', main(lists:reverse(Out))}, Ls};
+		true -> get_parentheses(Ls, [L | Out])
+	end.
+get_parentheses_nested([], _) ->
+	erlang:error(badarg);
+get_parentheses_nested([L | Ls], Out) ->
+	io:format("nest~n"),
+	if
+		L == $( ->
+			{A, B} = get_parentheses_nested(Ls, []),
+			get_parentheses_nested(A, [{'()', B} | Out]);
+		L == $) -> {Ls, {'()', main(lists:reverse(Out))}};
+		true -> get_parentheses_nested(Ls, [L | Out])
+	end.
 main(String) ->
 	main(String, []).
 main([], Out) ->
@@ -45,6 +69,9 @@ main([L | Ls] = List, Out) ->
 			catch
 				error:badarg -> main(Ls, Out)
 			end;
+		L == $( ->
+			{A, B} = get_parentheses(Ls),
+			main(B, [A | Out]);
 		L == $ -> main(Ls, Out);
 		true ->
 			try get_operator(List) of
@@ -55,8 +82,20 @@ main([L | Ls] = List, Out) ->
 				error:badarg -> main(Ls, Out)
 			end
 	end.
+
 main_test() ->
 [1,'+',2] = main("1+2"),
 [0.45,'/',6.5] = main("0.45/6.5"),
 [0,'*',1.567] = main("0*1.567"), 
-[5,'-',0] = main("5-0"). 
+[5,'-',0] = main("5-0"),
+[5,'*',{'()',[6,'/',4]},'+',{'()',[3,'*',4]}] = main("5*(6/4)+(3*4)").
+
+
+
+
+
+
+
+
+
+
