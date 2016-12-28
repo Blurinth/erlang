23282d11bf85ced24cdce1d0181e33ceb295d6de
Blurinth <olsonquinn1@gmail.com>
2016-12-28 18:54:23 -0500

    new parse2 function makes tuples from expressions with +, -, *, /

--- src/parsing.erl
+++ src/parsing.erl
@@ -1,6 +1,6 @@
 %% @author Quinn
 -module(parsing).
--export([main/1, stackcalc/1]).
+-export([parse/1, parse2/1, stackcalc/1]).
 -include_lib("eunit/include/eunit.hrl").
 
 get_number(String) ->
@@ -37,20 +37,20 @@ get_parentheses([L | Ls], Out, N) ->
 	if
 		L == $( -> get_parentheses(Ls, [L | Out], N + 1);
 		(L == $)) and (N > 0) -> get_parentheses(Ls, [L | Out], N - 1);
-		L == $) -> {{'()', main(lists:reverse(Out))}, Ls};
+		L == $) -> {{'()', parse(lists:reverse(Out))}, Ls};
 		true -> get_parentheses(Ls, [L | Out], N)
 	end.
-main(String) ->
-	main(String, []).
-main([], Out) ->
+parse(String) ->
+	parse(String, []).
+parse([], Out) ->
 	lists:reverse(Out);
-main([L | Ls] = List, Out) ->
+parse([L | Ls] = List, Out) ->
 	if
 		(L >= $0) and (L =< $9) ->
 			try get_number(List) of
 				_ ->
 					{A, B} = get_number(List),
-					main(B, [A | Out])
+					parse(B, [A | Out])
 			catch
 				error:badarg -> exit(baddeci)
 			end;
@@ -58,33 +58,33 @@ main([L | Ls] = List, Out) ->
 			try get_parentheses(Ls) of
 				_ ->
 					{A, B} = get_parentheses(Ls),
-					main(B, [A | Out])
+					parse(B, [A | Out])
 			catch
 				error:badarg -> exit(badparens)
 			end;
-		L == $ -> main(Ls, Out);
+		L == $ -> parse(Ls, Out);
 		true ->
 			try get_operator(List) of
 				_ ->
 					{A, B} = get_operator(List),
-					main(B, [A | Out])
+					parse(B, [A | Out])
 			catch
 				error:badarg -> exit(badsymbol)
 			end
 	end.
-main_test() ->
-	[1,'+',2] = main("1+2"),
-	[0.45,'/',6.5] = main("0.45/6.5"),
-	[0,'*',1.567] = main("0*1.567"),
-	[5,'-',0] = main("5-0"),
-	[5,'*',{'()',[6,'+',7]},'/',8] = main("5*(6+7)/8"),
-	[4,'*',{'()',[5,'-',{'()',[7,'/',8]}]}] = main("4*(5-(7/8))"),
-	[5,'*',{'()',[6,'/',4]},'+',{'()',[3,'*',4]}] = main("5*(6/4)+(3*4)"),
-	[1,'+',{'()',[2,'*',{'()',[4,'/',{'()',[5,'-',6]},'+',7]},'*',8]},'/',9] = main("1+(2*(4/(5-6)+7)*8)/9"),
-	?assertExit(badparens, main("(4+(3)")),
-	?assertExit(badsymbol, main("5+b6")),
-	?assertExit(baddeci, main("4.00.3 + 6.4")),
-	?assertExit(badparens, main("((4*(5)+6")).
+parse_test() ->
+	[1,'+',2] = parse("1+2"),
+	[0.45,'/',6.5] = parse("0.45/6.5"),
+	[0,'*',1.567] = parse("0*1.567"),
+	[5,'-',0] = parse("5-0"),
+	[5,'*',{'()',[6,'+',7]},'/',8] = parse("5*(6+7)/8"),
+	[4,'*',{'()',[5,'-',{'()',[7,'/',8]}]}] = parse("4*(5-(7/8))"),
+	[5,'*',{'()',[6,'/',4]},'+',{'()',[3,'*',4]}] = parse("5*(6/4)+(3*4)"),
+	[1,'+',{'()',[2,'*',{'()',[4,'/',{'()',[5,'-',6]},'+',7]},'*',8]},'/',9] = parse("1+(2*(4/(5-6)+7)*8)/9"),
+	?assertExit(badparens, parse("(4+(3)")),
+	?assertExit(badsymbol, parse("5+b6")),
+	?assertExit(baddeci, parse("4.00.3 + 6.4")),
+	?assertExit(badparens, parse("((4*(5)+6")).
 	
 stackcalc(Instr) ->
 	stackcalc(Instr, []).
@@ -100,8 +100,37 @@ stackcalc([I | Is], Stack) ->
 	stackcalc(Is, [I | Stack]);
 stackcalc([], [A]) ->
 	A.
+parse2(String) ->
+	parse2b(parse(String)).
+parse2b([L1, L2 | Ls]) ->
+	if
+		is_integer(L1) or is_float(L1) ->
+			if
+				L2 == '+' -> {'+', L1, parse2b(Ls)};
+				L2 == '-' -> {'-', L1, parse2b(Ls)};
+				L2 == '*' -> {'*', L1, parse2b(Ls)};
+				L2 == '/' -> {'/', L1, parse2b(Ls)}
+			end;
+		is_tuple(L1) ->
+			{_, A} = L1,
+			if
+				L2 == '+' -> {'()', [{'+', parse2b(A), parse2b(Ls)}]};
+				L2 == '-' -> {'()', [{'-', parse2b(A), parse2b(Ls)}]};
+				L2 == '*' -> {'()', [{'*', parse2b(A), parse2b(Ls)}]};
+				L2 == '/' -> {'()', [{'/', parse2b(A), parse2b(Ls)}]}
+			end
+	end;
+parse2b([L]) ->
+	L.
 
-		
+
+
+
+
+
+
+
+
 
 
 
